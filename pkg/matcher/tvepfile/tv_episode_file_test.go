2141a8d73b6562381a69143ae8f3304105dd375b
albert-hk <khuangustc@gmail.com>
2023-11-11 02:43:35 +0800

    add support for tmdbid

--- pkg/matcher/tvepfile/tv_episode_file_test.go
+++ pkg/matcher/tvepfile/tv_episode_file_test.go
@@ -65,3 +65,62 @@ func TestTvEpFileMatcher(t *testing.T) {
 
 	}
 }
+
+func TestTvEpFileMatcherTmdbid(t *testing.T) {
+	/// arrange
+	mockTmdbService := &tests.MockTmdbService{
+		ExpectedTv: map[int64]*common.MatchedTV{
+			123: {
+				MatchedCommon: common.MatchedCommon{
+					OriginalTitle: "XXX",
+					Year:          2021,
+					TmdbID:        123,
+				},
+			},
+		},
+	}
+	mockRenamerService := &tests.MockRenamer{
+		Expected: []renamer.RenameRecord{
+			{
+				Old: renamer.Path{
+					"montherpath",
+					"XXX.S01E02 tmdbid-123.mkv",
+				},
+				New: renamer.Path{
+					"targetpath",
+					"XXX (2021) [tmdbid-123]",
+					"Season 1",
+					"S01E02.mkv",
+				},
+			},
+		},
+	}
+	mockTargetService := &tests.MockTargetService{
+		TargetDirPath: "targetpath",
+	}
+	m, err := NewTvEpisodeFileMatcher(
+		"./tests/",
+		mockTmdbService,
+		mockRenamerService,
+		mockTargetService,
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	/// action
+	info := &common.Info{
+		DirPath: "montherpath",
+		Subs: []common.Single{
+			{
+				Name: "XXX.S01E02 tmdbid-123",
+				Ext:  ".mkv",
+				Size: 2 * 1024 * 1024 * 1024,
+			},
+		},
+	}
+	_, err = m.Match(info)
+	if err != nil {
+		t.Fatal(err)
+
+	}
+}
