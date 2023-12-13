9064816d0075bb25151c5ffcc6cc17f5a6f31b77
albert-hk <khuangustc@gmail.com>
2023-12-14 02:31:33 +0800

    slog now support relative path

--- internal/parser.go
+++ internal/parser.go
@@ -6,17 +6,24 @@ import (
 	"log/slog"
 	"os"
 	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+const (
+	mainFilePath = "/cmd/parser/main.go"
 )
 
 func PrepareLog() error {
 	appName := "parser"
 	logName := appName + ".log"
-	f, err := os.OpenFile(logName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	logFile, err := os.OpenFile(logName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		return fmt.Errorf("failed to open log file: %v", err)
 	}
-	mw := io.MultiWriter(os.Stderr, f)
-	basePath := "C:/Users/caskeep/mycode/project/2023Q4/asmediamgr"
+	mw := io.MultiWriter(os.Stderr, logFile)
+	_, filename, _, _ := runtime.Caller(1)
+	basePath := strings.TrimSuffix(filename, mainFilePath)
 	replace := func(groups []string, a slog.Attr) slog.Attr {
 		if a.Key == slog.SourceKey {
 			source := a.Value.Any().(*slog.Source)
