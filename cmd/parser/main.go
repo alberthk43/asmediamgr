4bd254583a93ed8547438fdf18b8cc03554d641e
albert-hk <khuangustc@gmail.com>
2023-12-14 03:21:35 +0800

    load configuration

--- cmd/parser/main.go
+++ cmd/parser/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	asmediamgr "asmediamgr/internal"
 	"asmediamgr/pkg/server"
+	"flag"
+	"fmt"
 
 	"log/slog"
 	"os"
@@ -13,17 +15,22 @@ const (
 )
 
 func main() {
+	flag.Parse()
 	if err := asmediamgr.PrepareLog(mainFilePath); err != nil {
 		slog.Error("Failed to prepare logging: %s", err)
 		os.Exit(1)
 	}
-	s, err := server.NewServer(&server.Configuration{})
+	c, err := server.LoadConfigurationFromFile(asmediamgr.Config)
 	if err != nil {
-		server.PrintAndDie(err.Error())
+		server.PrintAndDie(fmt.Sprintf("Failed to load config: %v", err))
+	}
+	s, err := server.NewServer(c)
+	if err != nil {
+		server.PrintAndDie(fmt.Sprintf("Failed to initialize server: %v", err))
 	}
 	err = server.Run(s)
 	if err != nil {
-		server.PrintAndDie(err.Error())
+		server.PrintAndDie(fmt.Sprintf("Failed to run server: %v", err))
 	}
 	s.WaitForShutdown()
 }
