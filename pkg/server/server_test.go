84561161cb4d1fdb1762c0a12dcacf7cbea6c06b
albert-hk <khuangustc@gmail.com>
2024-01-08 05:39:58 +0800

    bthdtv add 2nd dir name regex matcher (#25)

--- pkg/server/server_test.go
+++ pkg/server/server_test.go
@@ -1,6 +1,9 @@
 package server
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestSortParserInfoSlice(t *testing.T) {
 	parserInfoSlice := parserInfoSlice{
@@ -16,5 +19,16 @@ func TestSortParserInfoSlice(t *testing.T) {
 	}
 	if sorted[1].priority != 2 {
 		t.Fatal("sorted[1].priority != 2")
+	}
+}
+
+func TestNextRetryTime(t *testing.T) {
+	now, err := time.Parse(time.RFC3339, "2019-01-01T00:00:00Z")
+	if err != nil {
+		t.Fatal(err)
+	}
+	next := nextRetryTime(3, now)
+	if next.Sub(now) != time.Second*8 {
+		t.Fatal(err)
 	}
 }
